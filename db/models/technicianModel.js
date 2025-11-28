3969ce9c7ef2450715ddc87cf86bd2b313c6e292
yusuftarlan <you@example.com>
2025-11-28 17:05:42 +0300

    Eklendi: Yeni tekniker SQL sorguları

--- db/models/technicianModel.js
+++ db/models/technicianModel.js
@@ -1,6 +1,6 @@
 /*
 Bu modül techican_details tablosu için SQL komutlarını içerir
-*/ 
+*/
 import pool from "../config/db.js";
 
 //Tekniker detay kaydını oluşturur
@@ -20,7 +20,8 @@ export async function createTechnicianDetail(
     return rows;
 }
 
-export async function getTechnicianDetailbyID(tech_id) {
+//TeknikerID alarak tekniker detaylarını getirir
+export async function getTechnicianDetailByID(tech_id) {
     const [result] = await pool.query(
         `SELECT * FROM technician_details WHERE technician_id = ?`,
         tech_id
@@ -47,10 +48,82 @@ export async function setTechnicianNotAvailable(id) {
     return result;
 }
 
-export async function setTechnicianScore(id, score) {
+/*
+TEKNİKER SKORU İŞLEMLERİ
+*/
+export async function setTechnicianScore(tech_id, score) {
     const [result] = await pool.query(
         `UPDATE technician_details SET technician_score=? WHERE technician_id=?`,
-        [score,id]
+        [score, tech_id]
+    );
+    pool.end();
+    return result;
+}
+export async function updateTechnicianScore(tech_id) {
+    const [result] = await pool.query(
+        `UPDATE technician_details AS t 
+        SET t.technician_score = (SELECT AVG(s.service_score) FROM service_requests as s 
+        WHERE s.technician_id = t.technician_id) WHERE t.technician_id = ?`,
+        [tech_id]
+    );
+    pool.end();
+    return result;
+}
+
+//Tüm teknikerlerin id ve servis sayılarını getirir
+export async function getAllTechniciansServiceCount() {
+    const [result] = await pool.query(
+        `SELECT t.technician_id,
+            count(service_requests.id) FROM
+            service_requests as s JOIN
+            technician_details as t ON
+            s.technician_id = t.technician_id
+            GROUP BY t.technician_id`
+    );
+    pool.end();
+    return result;
+}
+//Bir alandaki ustaları puanlarına göre sıralayarak döndürür
+export async function getTechniciansScoreByProfession(profession) {
+    const [result] = await pool.query(
+        `SELECT u.name, u.surname,
+        t.technician_score FROM
+        technician_details as t JOIN
+        users AS u
+        ON t.technician_id = u.id WHERE
+        t.profession = ? ORDER BY
+        t.technician_score DESC `,
+        [profession]
+    );
+    pool.end();
+    return result;
+}
+
+//Bir alandaki ustaları deneyimlerine göre sıralayarak döndürür
+export async function getTechniciansExpYearByProfession(profession) {
+    const [result] = await pool.query(
+        `SELECT u.name, u.surname, t.
+        experience_years
+        FROM technician_details as t
+        JOIN users AS u
+        ON t.technician_id = u.id WHERE
+        t.profession = ? ORDER BY t.
+        experience_years
+        DESC`,
+        [profession]
+    );
+    pool.end();
+    return result;
+}
+
+//10'dan fazla şikayet almış ustaları döndürür
+export async function getAllTechniciansTenTimesGetComplained() {
+    const [result] = await pool.query(
+        `SELECT sr.technician_id, u.first_name, u.surname, COUNT(c.id) AS complaint_count
+        FROM complaints c JOIN service_requests sr ON c.request_id = sr.id
+        JOIN users u ON sr.technician_id = u.id
+        GROUP BY sr.technician_id, u.first_name, u.surname
+        HAVING COUNT(c.id) > 10;`
     );
     pool.end();
     return result;
